6386fcd823432c57dc448639f076ea148b604510
Youssef Nafaa <nafaayoussef@gmail.com>
2022-11-03 16:03:44 -0400

    first component Employee

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
+      <Employee />
     </div>
   );
 }
---
+++ src/components/Employee.jsx
@@ -0,0 +1,5 @@
+const Employee = () => {
+	return <h2>Emplyee Component</h2>;
+};
+
+export default Employee;
